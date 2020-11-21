f09fe77ea110aa3128b62b79dd46f19961eb3d58
dedykaryadigunawan <dedykaryadigunawan@gmail.com>
2020-11-21 18:48:11 +0700

    Tugas Hewan Pedia

---
+++ app/src/main/java/simple/example/hewanpedia/model/Kelinci.java
@@ -0,0 +1,11 @@
+package simple.example.hewanpedia.model;
+
+
+public class Kelinci extends Hewan {
+
+
+    public Kelinci(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Kelinci",ras,asal,deskripsi,drawableRes);
+    }
+
+}
